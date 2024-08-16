26f36aad6f9082a3ac812bf8591653e4a5d3e898
EduRocha15 <ceduardo486@gmail.com>
2024-08-15 21:53:13 -0300

    aula sobre componentes

---
+++ a4_introReact/fundamentos/src/app/components/Cabecalho.js
@@ -0,0 +1,10 @@
+export default function Cabecalho () {
+    return (
+        <>
+            <div>
+                <h1>Cabeçalho</h1>
+                <p>Isto é um cabeçalho!</p>
+            </div>
+        </>
+    )
+}
---
+++ a4_introReact/fundamentos/src/app/fundamentos/page.js
@@ -0,0 +1,20 @@
+export default function Fundamentos () {
+
+    function Cabecalho () {
+        return (
+            <>
+                <div>
+                    <h1>Cabeçalho</h1>
+                    <p>Isto é um cabeçalho!</p>
+                </div>
+            </>
+        )
+    }
+
+    return (
+        <>
+            <h1>Fundamentos</h1>
+            <p>Sucesso!</p>
+        </>
+    )
+}
